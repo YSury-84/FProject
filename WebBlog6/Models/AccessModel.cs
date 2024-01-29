10f7e01ae60ff344d5b7a22b7455ee160e08a701
YSury-84 <Yury.Kolchurin@evraz.com>
2024-01-29 20:11:04 +0700

    Модернезирована форма доступа к системе: label-display и validation

--- WebBlog6/Models/AccessModel.cs
+++ WebBlog6/Models/AccessModel.cs
@@ -4,9 +4,12 @@ namespace WebBlog6.Models
 {
     public class AccessModel
     {
+        [Display(Name = "Логин:")]
         [Required(ErrorMessage = "Необходимо ввести имя")]
         public string Login { get; set; }
+        [Display(Name = "Пароль:")]
         [Required(ErrorMessage = "Необходимо ввести пароль")]
+        [StringLength(32,MinimumLength = 4, ErrorMessage = "Необходимо ввести хотя бы 4-ре символа")]
         public string Pass { get; set; }
     }
 }
