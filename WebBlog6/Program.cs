392da2519bb42c553712abd316a79a2f15d7c88a
YSury-84 <Yury.Kolchurin@evraz.com>
2024-02-08 19:27:02 +0700

    ConnectionString - ушла в конфигурацию.

--- WebBlog6/Program.cs
+++ WebBlog6/Program.cs
@@ -12,8 +12,8 @@ namespace WebBlog6
             var builder = WebApplication.CreateBuilder(args);
 
             // получаем строку подключения из файла конфигурации
-            //string connection = builder.Configuration.GetConnectionString("DefaultConnection");
-            string connection = "Data Source='C:\\Users\\student\\source\\repos\\YSury-84\\FProject\\DBLite\\blog.db';Mode=ReadWriteCreate;Cache=Shared";
+            string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+            //string connection = "Data Source='C:\\Users\\student\\source\\repos\\YSury-84\\FProject\\DBLite\\blog.db';Mode=ReadWriteCreate;Cache=Shared";
 
             // добавляем контекст ApplicationContext в качестве сервиса в приложение
             builder.Services.AddDbContext<DataContext>(options => options.UseSqlite(connection));
